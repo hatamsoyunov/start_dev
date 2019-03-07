8faea24fbbf2defa7238d79ef4a447595cf3fad4
hatamsoyunov <hattam@mail.ru>
2019-03-07 20:50:35 +0300

    replace '_' > 'space'

--- app/mail/mail.php
+++ app/mail/mail.php
@@ -9,6 +9,7 @@ if ( $method === 'POST' ) {
 	$form_subject = trim($_POST["form_subject"]);
 	foreach ( $_POST as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "from_email" && $key != "form_subject" ) {
+			$key = str_replace('_', ' ', $key);
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
@@ -24,6 +25,7 @@ if ( $method === 'POST' ) {
 	$form_subject = trim($_GET["form_subject"]);
 	foreach ( $_GET as $key => $value ) {
 		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "from_email" && $key != "form_subject" ) {
+			$key = str_replace('_', ' ', $key);
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
